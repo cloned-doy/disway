f0e7153b7e98e1c78dc199911b5d90fcad6ea563
doy <doy@doy.doy>
2024-02-16 09:39:41 +0700

    fix code readability issue

--- get_links.py
+++ get_links.py
@@ -1,71 +1,54 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-import time
+from time import sleep
 
-url = "https://disway.id/kategori/99/catatan-harian-dahlan/"
+#fetch the HTML content of the base url and get all the blog post links
+def get_links(links, base_url):
 
-links = []  # Initialize an empty list to store the links
+	if links is None: links = []
+	response = requests.get(base_url)
 
-# Send an HTTP GET request to fetch the HTML content of the webpage
-
-def get_links(links, url):
-	response = requests.get(url)
-
-	# Check if the request was successful
 	if response.status_code == 200:
-		# Parse the HTML content with BeautifulSoup
 		soup = BeautifulSoup(response.text, 'html.parser')
 
 		# Find all anchor tags within the specified div class ("bottom-15")
 		link_divs = soup.find_all('h2', class_='media-heading')
-
-		# Extract links from each div
 		new_links = [div.find('a')['href'] for div in link_divs if div.find('a')]
-
-		if links is None:
-			links = []
-
-		# Append new_links to links
 		links.extend(new_links)
 		
-		return links
+		print(f"total new links: {len(links)}")
+	
+	return links
+	
 
+def main(base_url, page_total, blog_map_path):
 
-base_link = "https://disway.id/kategori/99/catatan-harian-dahlan/"
+	print("base url: "+base_url)
+	print("start getting the post links")
 
+	links = []  # Initialize an empty list to store the links
+	for denom in range(page_total):
+		post_id = denom * 30 # 30 is the default total post of each page
+		url_link = base_url + str(post_id) if post_id != 0 else base_url # Use a conditional expression to handle the concatenation
+		get_links(links, url_link)
+		# if denom / 10 == 0 : sleep(30) # dumb line to make the requests fecth block safe
 
-for denom in range(45):
-	# if denom / 5 == 0:
-	# 	time.sleep(3)
-	angka = denom * 30
+	# Convert the links list into a dictionary with float keys
+	blog_map = {float(i+1): link for i, link in enumerate(links)}
 
-	# Use a conditional expression to handle the concatenation
-	url_link = base_link + str(angka) if angka != 0 else base_link
+	# Save the blog_map dictionary into an updatable JSON file
+	with open(blog_map_path, 'w') as f:
+		json.dump(blog_map, f)
+		
+	print("link gathering is done.")
+	# print(blog_map)
+	print(f"total links: {len(links)}")
 
-	get_links(links, url_link)
+if __name__ == "__main__":
 
-print(links)
+	base_url = "https://disway.id/kategori/99/catatan-harian-dahlan/" # base url disway
+	page_total = 76 # total page of disway blog. per page is 30 posts
+	blog_map_path = "blog_map.json" # json to save blog post links
 
-# Convert the links list into a dictionary with float keys
-blog_map = {float(i+1): link for i, link in enumerate(links)}
-
-print(blog_map)
-
-# Save the blog_map dictionary into an updatable JSON file
-json_file_path = 'blog_map.json'
-with open(json_file_path, 'w') as f:
-	json.dump(blog_map, f)
-
-# Now you can read the JSON file and update the blog_map if needed
-# Load the blog_map from the JSON file with keys converted back to float
-def float_keys_hook(obj):
-    return {float(key): value for key, value in obj.items()}
-
-json_file_path = 'blog_map.json'
-with open(json_file_path, 'r') as f:
-    updated_blog_map = json.load(f, object_hook=float_keys_hook)
-
-# You can now use the updated_blog_map dictionary with float keys as needed
-print(updated_blog_map)
-
+	main(base_url, page_total, blog_map_path)
